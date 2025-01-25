5becc0ca1f294fa9f5636bb003c249bd61d65a63
Sergey Chernoskutov <chernoskytofff@gmail.com>
2025-01-25 15:47:58 +0500

    feat: updating book

--- src/routes/book.js
+++ src/routes/book.js
@@ -22,5 +22,45 @@ module.exports = (store) => {
         });
     });
 
+    router.post('/:id/update', (req, res) => {
+        const { id }      = req.params;
+        const {
+                  title,
+                  authors,
+                  description,
+                  favorite,
+                  fileCover,
+                  fileName,
+              }           = req.body;
+        const updatedBook = {};
+
+        if (title !== undefined) {
+            updatedBook.title = title;
+        }
+        if (authors !== undefined) {
+            updatedBook.authors = authors;
+        }
+        if (description !== undefined) {
+            updatedBook.description = description;
+        }
+        if (favorite !== undefined) {
+            updatedBook.favorite = favorite;
+        }
+        if (fileCover !== undefined) {
+            updatedBook.fileCover = fileCover;
+        }
+        if (fileName !== undefined) {
+            updatedBook.fileName = fileName;
+        }
+
+        const book = store.updateBook(updatedBook, id);
+        if (book) {
+            res.redirect(`/book/${book.id}`);
+        }
+        else {
+            res.status(404);
+        }
+    });
+
     return router;
 };
