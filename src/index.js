4e40248a6edfdedaf2bad1f388ae59266e4a18d6
Sergey Chernoskutov <chernoskytofff@gmail.com>
2025-01-25 13:08:03 +0500

    feat: add templates for layout and create main page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-const express        = require('express');
-const { v4: uuid }   = require('uuid');
+const express         = require('express');
+const errorMiddleware = require('./middleware/error');
 const apiUserRouter  = require('./routes/api/user.js');
 const apiBooksRouter = require('./routes/api/books.js');
-const Store          = require('./Store.js');
+const indexRouter = require('./routes/index');
+const bookRouter = require('./routes/book');
+const Store       = require('./Store.js');
 
 const store = new Store();
 const app   = express();
 
+app.use(express.urlencoded());
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+app.use('/', indexRouter);
+app.use('/book', bookRouter);
+
 app.use(express.json());
 app.use('/api/user', apiUserRouter);
 app.use('/api/books', apiBooksRouter(store));
+
+
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
