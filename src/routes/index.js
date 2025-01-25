4b7f69479135dd5e3e2f0e5a16cba2961c001355
Sergey Chernoskutov <chernoskytofff@gmail.com>
2025-01-25 15:33:27 +0500

    feat: creating book

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Book    = require('../Book');
 const router  = express.Router();
 
 module.exports = (store) => {
@@ -14,6 +15,29 @@ module.exports = (store) => {
             title: 'New book',
             book : {},
         });
+    });
+
+    router.post('/create', (req, res) => {
+        const {
+                  title,
+                  authors,
+                  description = '',
+              } = req.body;
+
+        if (title && authors) {
+            const newBook = new Book({
+                title,
+                authors,
+                description,
+            });
+            store.addBook(newBook);
+
+            res.status(201);
+            res.redirect(`/book/${newBook.id}`);
+        }
+        else {
+            res.status(400);
+        }
     });
 
     return router;
