88a3c7a3700c50f23219d12201e19f3e4d1cc8a8
Sergey Chernoskutov <chernoskytofff@gmail.com>
2025-03-22 13:00:18 +0500

    feat: add post signup

--- bookshelf/src/routes/api/user/user.router.js
+++ bookshelf/src/routes/api/user/user.router.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router  = express.Router();
 
-router.get('/login', (req, res) => {
+router.post('/login', (req, res) => {
     res.status(201);
     res.json({ id: 1, mail: 'test@mail.ru' });
 });
+
+router.post('/signup', (req, res) => {
+    // todo
+})
 
 module.exports = router;
