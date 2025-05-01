53a3a875a642014201c02a7a7089e372a4ded95d
Sergey Chernoskutov <chernoskytofff@gmail.com>
2025-05-01 09:25:44 +0500

    feat: create interface for Book

---
+++ bookshelf/src/models/Book.d.ts
@@ -0,0 +1,9 @@
+export interface Book {
+    title : string,
+    authors : string,
+    description? : string,
+    fileCover? : string,
+    fileName? : string,
+    fileBook? : string,
+    favorite? : boolean,
+}
