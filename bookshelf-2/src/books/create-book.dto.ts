a14e648e844d6e19b6a260a458a1a301b92d86b3
Sergey Chernoskutov <chernoskytofff@gmail.com>
2025-07-18 10:08:05 +0500

    feat: validation

--- bookshelf-2/src/books/create-book.dto.ts
+++ bookshelf-2/src/books/create-book.dto.ts
@@ -1,9 +1,33 @@
+import { IsString, IsInt, Max, Length, IsNotEmpty, IsOptional } from 'class-validator';
+
 export class CreateBookDto {
+    @IsString()
+    @IsNotEmpty()
+    @Length(1, 255)
     title : string;
+
+    @IsOptional()
+    @IsInt()
+    @Max(new Date().getFullYear())
     year ? : number;
+
+    @IsString()
     description : string;
+
+    @IsString()
+    @Length(1, 255)
+    @IsNotEmpty()
     authors : string;
+
+    @IsOptional()
+    @IsString()
     fileCover ? : string;
+
+    @IsOptional()
+    @IsString()
     fileName ? : string;
+
+    @IsOptional()
+    @IsString()
     fileBook ? : string;
 }
