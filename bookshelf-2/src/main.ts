4059517a62dce2beffdf522151e1b3c01bcdae79
Sergey Chernoskutov <chernoskytofff@gmail.com>
2025-07-19 18:16:42 +0500

    feat: validation

--- bookshelf-2/src/main.ts
+++ bookshelf-2/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory }    from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { env }            from './config/env';
 import { AppModule }      from './app.module';
 
 async function bootstrap () {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
     await app.listen(env.PORT ?? 3000);
     console.log('App running on port ', env.PORT);
 }
